31ec57719f7430832aca87a9566c660bb4cd59ba
Egor Tarasov <vosarat1995@google.com>
2026-02-04 11:19:02 +0100

    Playground Project Scaffolded

---
+++ dotnet-new/play/SingleLineConsole.cs
@@ -0,0 +1,9 @@
+﻿namespace Copaster.DotnetNew.Playground;
+
+public static class SingleLineConsole
+{
+    extension(ILoggingBuilder logging)
+    {
+        public void AddSingleLineConsole() => logging.AddSimpleConsole(c => c.SingleLine = true);
+    }
+}
