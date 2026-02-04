4299de879e61c8ddd351195cfdd67cf85a1946d6
Egor Tarasov <vosarat1995+1@gmail.com>
2026-02-04 13:39:03 +0100

    dontet-new: Base Play (#1)

---
+++ dotnet-new/play/source/SingleLineConsole.cs
@@ -0,0 +1,11 @@
+﻿global using Copaster.DotnetNew.Playground.Source;
+
+namespace Copaster.DotnetNew.Playground.Source;
+
+public static class SingleLineConsole
+{
+    extension(ILoggingBuilder logging)
+    {
+        public void AddSingleLineConsole() => logging.AddSimpleConsole(c => c.SingleLine = true);
+    }
+}
---
+++ dotnet-new/play/target/SingleLineConsole.cs
@@ -0,0 +1,11 @@
+﻿global using target;
+
+namespace target;
+
+public static class SingleLineConsole
+{
+    extension(ILoggingBuilder logging)
+    {
+        public void AddSingleLineConsole() => logging.AddSimpleConsole(c => c.SingleLine = true);
+    }
+}
