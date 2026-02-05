d72407dad9b479b6afbc65f3c7081a0a8df682fb
Egor Tarasov <vosarat1995@google.com>
2026-02-05 14:21:24 +0100

    fix-ns tool: Setup of the tool

---
+++ namespaces/cli/Program.cs
@@ -0,0 +1,8 @@
+﻿var cwd = Directory.GetCurrentDirectory();
+Console.WriteLine("Hello, I fix namespaces!");
+Console.WriteLine($"Current working directory: {cwd}");
+Console.WriteLine("Contents:");
+foreach (var entry in Directory.EnumerateFileSystemEntries(cwd))
+{
+    Console.WriteLine(entry);
+}
