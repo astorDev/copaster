72e9a035b9f27816b0f53f0e53c9aed0fa12770a
Egor Tarasov <vosarat1995@google.com>
2026-08-01 14:03:20 +0200

    copy: Skeleton

---
+++ copy/cli/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = new CliBuilder();
+
+builder.Logging.AddNiceShell();
+
+builder.AddCommand<CopyCommand>();
+
+using var app = builder.Build("A copaster.copy CLI application.");
+
+return app.Run(args);
