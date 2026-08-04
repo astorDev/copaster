c81f728b5a6b70f54da94954033ebf77b015d0fa
Egor Tarasov <vosarat1995+1@gmail.com>
2026-08-04 08:29:15 +0200

    replace/cli: `--all-cases` Support + Update to Use Nice-Shell (#16)
    * replace/cli: V2 Initial Take
    
    * replace/cli: Finalizing on Updated version
    
    ---------
    
    Co-authored-by: Egor Tarasov <vosarat1995@google.com>

--- replace/cli/Program.cs
+++ replace/cli/Program.cs
@@ -1,39 +1,9 @@
-﻿using System.CommandLine;
-using Copaster.Replace;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿var builder = new CliBuilder();
 
-Option<string> folderOption = new("--folder")
-{
-    Description = "The folder to apply replacements in (defaults to current directory)",
-    DefaultValueFactory = _ => Directory.GetCurrentDirectory()
-};
-folderOption.Aliases.Add("-f");
+builder.Logging.AddNiceShell();
 
-Argument<string> fromArgument = new("from") { Description = "The text to replace" };
-Argument<string> toArgument = new("to") { Description = "The replacement text" };
+builder.AddAsRootCommand<ReplaceCommand>();
 
-RootCommand rootCommand = new("Replace literal symbols across file contents and file/folder names");
-rootCommand.Options.Add(folderOption);
-rootCommand.Arguments.Add(fromArgument);
-rootCommand.Arguments.Add(toArgument);
+using var app = builder.Build();
 
-rootCommand.SetAction(parseResult =>
-{
-    var serviceCollection = new ServiceCollection();
-    serviceCollection.AddLogging(l => l.AddSimpleConsole(c => c.SingleLine = true));
-    var services = serviceCollection.BuildServiceProvider();
-    var logger = services.GetRequiredService<ILogger<Program>>();
-
-    var folder = parseResult.GetValue(folderOption)!;
-    var from = parseResult.GetValue(fromArgument)!;
-    var to = parseResult.GetValue(toArgument)!;
-
-    var resolvedFolder = Path.IsPathRooted(folder) ? folder : Path.Combine(Directory.GetCurrentDirectory(), folder);
-    logger.LogInformation("Replacing '{From}' -> '{To}' in {Folder}", from, to, resolvedFolder);
-    Replacer.ReplaceInFolder(resolvedFolder, from, to);
-    logger.LogInformation("Done");
-    return 0;
-});
-
-return rootCommand.Parse(args).Invoke();
+return app.Run(args);
--- replace/example/OriginalSymbols.cs
+++ replace/example/OriginalSymbols.cs
@@ -3,4 +3,11 @@ public class OriginalSymbols
     public const string Symbol1 = "Symbol1";
     public const string Symbol2 = "Symbol2";
     public const string Symbol3 = "Symbol3";
+
+    public const string InPascal = "OriginalSymbols";
+    public const string InCamel = "originalSymbols";
+    public const string InKebab = "original-symbols";
+    public const string InSnake = "original_symbols";
+    public const string InScreamingSnake = "ORIGINAL_SYMBOLS";
+    public const string InScreamingKebab = "ORIGINAL-SYMBOLS";
 }
