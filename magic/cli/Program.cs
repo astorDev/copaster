174fadbc9f753fe3600a1c1d8ecbcecf6beee441
Egor Tarasov <vosarat1995@google.com>
2026-09-04 08:16:31 +0200

    magic/cli: Skeleton

---
+++ magic/cli/Program.cs
@@ -0,0 +1,11 @@
+﻿using Copaster;
+
+var builder = new CliBuilder();
+
+builder.Logging.AddNiceShell();
+
+builder.AddAsRootCommand<MagicCommand>();
+
+using var app = builder.Build("A magic CLI application.");
+
+return app.Run(args);
